192da9b8adf6ecc10631264e4e5d10239472d065
jspieckermann <jspieckermann@web.de>
2022-02-11 15:51:44 +0100

    first commit

---
+++ src/main/java/org/spieckermann/skateboarding/SkateboardingApplication.java
@@ -0,0 +1,13 @@
+package org.spieckermann.skateboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkateboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkateboardingApplication.class, args);
+	}
+
+}
